b5fd837858a0811fd1ea80ec16c953eabb98ad22
Ahmedov Ravshanbek <ravshanbek.121@gmail.com>
2016-07-05 17:54:05 +0500

    This is a new commit for what I originally planned to be amended

--- GetTweetLinks.php
+++ GetTweetLinks.php
@@ -3,13 +3,20 @@
 /*
 
   What is wrong with this code?
-  1. урл неправильно получается скрипт должен брать из массив entities->media
+  1. урл неправильно получается скрипт должен брать из массив entities->media->0->url
 
 */
 
 	require_once('class.php'); 
+
+	$set = array(
+	        'oauth_access_token'			=>	"3303873794-iqbs0qCiaUeTWDPRbndGwinlnbQw8WtUpUqK0Ec",
+	        'oauth_access_token_secret'		=>	"iEbx96c1zlE1y7PTZ6qLVY8cKuN7ZGYPRcCPa3i0gzCGZ",
+	        'consumer_key'					=>	"koE5W7SByX0fW2udhcksnfMZQ",
+	        'consumer_secret'				=>	"DIaxgL6Yssi9VqJnycgmE2fucxDH9XYhM4E5qQ8xeLw8U3kNDA"
+	);
 	
-	$twitt = new twitt;
+	$twitt = new twitt($set);
 	$twitt->getLinkByUserName("lewishamilton");
 
 ?>
--- class.php
+++ class.php
@@ -6,15 +6,8 @@ class twitt {
 
 	public $twAPI;
 
-	public function __construct()
-	{
-		$set = array(
-		        'oauth_access_token'			=>	"3303873794-iqbs0qCiaUeTWDPRbndGwinlnbQw8WtUpUqK0Ec",
-		        'oauth_access_token_secret'		=>	"iEbx96c1zlE1y7PTZ6qLVY8cKuN7ZGYPRcCPa3i0gzCGZ",
-		        'consumer_key'					=>	"koE5W7SByX0fW2udhcksnfMZQ",
-		        'consumer_secret'				=>	"DIaxgL6Yssi9VqJnycgmE2fucxDH9XYhM4E5qQ8xeLw8U3kNDA"
-		);
-		
+	public function __construct($set = array())
+	{	
 		$this->twAPI = new TwitterAPIExchange($set);		
 	}
 	
@@ -33,7 +26,7 @@ class twitt {
 
 			if($url = $one["entities"]["media"][0]["url"])
 			{
-				echo  $url."<br/>";
+				echo $url."<br/>";
 			} else {
 				echo "NOT FOUND!!!<br/>";
 		    }
